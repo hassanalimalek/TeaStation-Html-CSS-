7d6851beaf84aa1ce191c5cafb3f9ff45c45bf76
Hassan Ali <hassanalimalek9@gmail.com>
2020-08-29 18:41:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const navbtn = document.getElementById('nav-btn')
+const navbar = document.getElementById('navbar')
+const navClose = document.getElementById('nav-close')
+
+navbtn.addEventListener('click',()=> {
+    navbar.classList.add("showNav");
+});
+
+navClose.addEventListener('click',()=> {
+    navbar.classList.remove("showNav");
+});
